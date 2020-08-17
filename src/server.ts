f9ceccee47c297735066bb4ffd94147338f277f8
Davidson Oliveira <dpeixoto.cc@gmail.com>
2020-08-18 00:15:31 +0100

    Seting up project structure

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { json } from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(json());
+app.use(routes);
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('🚀🇧🇷 App running on 3333 Port....');
+});
